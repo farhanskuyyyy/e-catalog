acd171386d075fbc53d5d19630a8f6394d8bf97b
farhanlumoshive <farhan.arfianto@lumoshive.com>
2024-07-03 22:54:34 +0700

    feat: add stisla template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,88 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+@extends('layouts.auth')
+
+@section('title', 'Login')
+
+@section('main')
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Login</h4>
+        </div>
+
+        <div class="card-body">
+            <form method="POST"
+                action="{{ route('login') }}"
+                class="needs-validation"
+                novalidate="">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email"
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        tabindex="1"
+                        required
+                        autofocus>
+                    <div class="invalid-feedback">
+                        Please fill in your email
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="d-block">
+                        <label for="password"
+                            class="control-label">Password</label>
+                        {{-- <div class="float-right">
+                            <a href="auth-forgot-password.html"
+                                class="text-small">
+                                Forgot Password?
+                            </a>
+                        </div> --}}
+                    </div>
+                    <input id="password"
+                        type="password"
+                        class="form-control"
+                        name="password"
+                        tabindex="2"
+                        required>
+                    <div class="invalid-feedback">
+                        please fill in your password
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox"
+                            name="remember"
+                            class="custom-control-input"
+                            tabindex="3"
+                            id="remember-me">
+                        <label class="custom-control-label"
+                            for="remember-me">Remember Me</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit"
+                        class="btn btn-primary btn-lg btn-block"
+                        tabindex="4">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+    {{-- <div class="text-muted mt-5 text-center">
+        Don't have an account? <a href="auth-register.html">Create One</a>
+    </div> --}}
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+@endpush
